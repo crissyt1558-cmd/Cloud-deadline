5dbb83f55b808cb42220984f4a8083b291ba2bbc
Otiwa <otiwaachr@yahoo.com>
2026-02-10 16:49:56 -0500

    Rename sendNotifications.js to .cjs

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDdmyDjgpOOu54uZZ0xR9_Yqj6FQDfMITM",
+  authDomain: "duetrack-f2f53.firebaseapp.com",
+  projectId: "duetrack-f2f53",
+  storageBucket: "duetrack-f2f53.firebasestorage.app",
+  messagingSenderId: "883900074594",
+  appId: "1:883900074594:web:66a9963918fa220c3a9f3b",
 });
 
 const messaging = firebase.messaging();
